89457ac9b72f88c96813dc76fcb0ade13731fcb9
Lansingcode <1406063770@qq.com>
2018-06-20 16:57:05 +0800

    add file data split

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 __author__ = 'xujia'
 
 import pandas as pd
+import numpy as np
 
 
 def fileInfo(path):
     '''
     获取文件信息
     :param path: 文件路径
-    :return: {字段名称：[字段类型，数据量，空值数]}
+    :return: {字段名称：[字段类型，数据量，空值个数]}
     '''
     infodict = {}
     data = pd.read_csv(path)
@@ -17,10 +18,29 @@ def fileInfo(path):
         ctype = data[c].dtype
         nc = data[c].size - data[c].notnull().sum()
         infodict[c] = [ctype, data[c].size, nc]  # 字段类型，数据量，空值个数
-    return infodict
+    return infodict, data
+
+
+def dataSplit(data, ratio):
+    '''
+    数据分割
+    :param data:带分割数据
+    :param ratio: 分割比例
+    :return: （数据集1，数据集2）
+    '''
+    dataCount = data.shape[0]
+    selectedCount = int(dataCount * ratio)
+    if selectedCount > 0:
+        splitedData = np.split(data.sample(frac=1), [selectedCount], axis=0)
+    else:
+        return 'Data is too less'
+    return splitedData
 
 
 if __name__ == '__main__':
     # path=input('Please input the file path: ')
     path = 'iris.csv'
-    ret = fileInfo(path)
+    dict, data = fileInfo(path)
+    t = dataSplit(data, 0.8)
+    print(t[0])
+    print(t[1])
