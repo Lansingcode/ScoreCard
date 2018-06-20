fbaba274cab4e46feb958246776267fff8cfe3f8
Lansingcode <1406063770@qq.com>
2018-06-20 13:41:41 +0800

    add iris data

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # -*- coding:utf-8 -*-
+import pandas as pd
 
+def fileIO(path):
+    data=pd.read_csv(path)
+    print(data.columns)
+    print(data.describe())
 
 
 if __name__=='__main__':
-    pass
+    path=input('Please input the file path: ')
+    fileIO(path)
