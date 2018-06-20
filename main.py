2e826c0ef4b8604aa9b5496cade6bb883bb31b0c
Lansingcode <1406063770@qq.com>
2018-06-20 14:15:07 +0800

    add file information

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 # -*- coding:utf-8 -*-
+__author__ = 'xujia'
+
 import pandas as pd
 
-def fileIO(path):
-    data=pd.read_csv(path)
-    print(data.columns)
-    print(data.describe())
+
+def fileInfo(path):
+    '''
+    获取文件信息
+    :param path: 文件路径
+    :return: {字段名称：[字段类型，数据量，空值数]}
+    '''
+    infodict = {}
+    data = pd.read_csv(path)
+    for c in data.columns:
+        infodict[c] = data[c].dtype
+        ctype = data[c].dtype
+        nc = data[c].size - data[c].notnull().sum()
+        infodict[c] = [ctype, data[c].size, nc]  # 字段类型，数据量，空值个数
+    return infodict
 
 
-if __name__=='__main__':
-    path=input('Please input the file path: ')
-    fileIO(path)
+if __name__ == '__main__':
+    # path=input('Please input the file path: ')
+    path = 'iris.csv'
+    ret = fileInfo(path)
