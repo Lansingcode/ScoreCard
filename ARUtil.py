72c28ae051b9e150cdba7d0ba1118894f9f43424
GiantTao <wtctc@126.com>
2018-06-20 16:36:35 +0800

    Update ARUtil.py
    注释编码

--- ARUtil.py
+++ ARUtil.py
@@ -2,9 +2,9 @@
 import pandas as pd
 import numpy as np
 import logging
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 
 
 class ARFilter(object):
