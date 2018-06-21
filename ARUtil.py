823958a3170820b6d1b81099d10a852b77bb7efa
GiantTao <wtctc@126.com>
2018-06-21 10:27:33 +0800

    流程化处理文件读取、空值填充

--- ARUtil.py
+++ ARUtil.py
@@ -107,43 +107,36 @@ class ARFilter(object):
         self.logger.info(pd.DataFrame(final_list))
         pd.DataFrame(final_list, columns=['varName', 'AR']).to_excel('result.xlsx', index=False)
 
-    def fill_empty_value(self, col_name, file_name='input.xls', default_value=0):
+    def fill_empty_value(self, col_name, data, default_value=0):
         """
         缺失值填充
         输入：宽表【变量1、变量2、目标变量】，变量名称，缺失值填充值（默认0）
         计算方式：直接将指定变量中的缺失值用参数中的填充值进行填充
         输出：填充后的宽表，变量缺失率
         """
-        data = pd.read_excel(file_name)
-        # print(str(np.nan))
-        # print(type(str(np.nan)))
-        # print type(str(data['emptyCol'][14]))
-        # print len(str(data['emptyCol'][14]).strip())
-        # print type(str(data['emptyCol'][14]).strip())
+        # data = pd.read_excel(file_name)
         if col_name not in data.columns.values:
             self.logger.error("输入宽表中不存在指定变量")
             return
         else:
-            empty_count = data.shape[0] - data[col_name].count()
+            empty_count = data[col_name].shape[0] - data[col_name].count()
             if empty_count > 0:
                 self.logger.info('当前共' + str(data.shape[0]) + '个变量值，其中缺失值个数为' + str(empty_count))
                 # 替换空串为NAN
-                # data[col_name] = data[col_name].replace(' ', np.nan).fillna(value=default_value)
-                data['result'] = data[col_name].replace(' ', np.nan).fillna(value=default_value)
-                # self.logger.info('填补后，缺失值个数为' + str(data.shape[0] - data[col_name].count()))
-                self.logger.info('填补后，缺失值个数为' + str(data.shape[0] - data['result'].count()))
-                data.to_excel('result.xls', index=False)
+                data[col_name] = data[col_name].replace(' ', np.nan).fillna(value=default_value)
+                self.logger.info('填补后，缺失值个数为' + str(data[col_name].shape[0] - data[col_name].count()))
+                # data.to_excel('result.xls', index=False)
+                return data
             else:
                 self.logger.info('当前不存在缺失值')
 
-    def del_empty_value(self, file_name='input.xls', empty_rate_threshold=0.5):
+    def del_empty_value(self, data, empty_rate_threshold=0.5):
         """
         缺失值剔除
         输入：宽表【变量1、变量2、目标变量】，缺失率（默认0.5）
         计算方式：计算宽表中各个变量的缺失率，并剔除缺失率超过0.5的变量
         输出：处理后宽表
         """
-        data = pd.read_excel(file_name)
         for col in data.columns.values:
             if col == 'y':
                 continue
@@ -151,14 +144,77 @@ class ARFilter(object):
             if empty_ratio >= empty_rate_threshold:
                 self.logger.info("变量：" + col + "缺失率为" + str(empty_ratio) + ",高于阈值：" + str(empty_rate_threshold))
                 data = data.drop(col, axis=1)
-        data.to_excel(file_name.split(".")[0] + "_new." + file_name.split(".")[1], index=False)
+        return data
+        # data.to_excel(file_name.split(".")[0] + "_new." + file_name.split(".")[1], index=False)
+
+    def console_input(self, prompt="", if_value=[], else_value=[], if_rtn="", else_rtn=""):
+        rtn = input(prompt)
+        if rtn.strip() in if_value:
+            return if_rtn
+        elif rtn.strip() in else_value or len(else_value) == 0:
+            return else_rtn
+        else:
+            raise IOError("未匹配到条件")
+
+    def file_info(self, path):
+        """
+        获取文件信息
+        :param path: 文件路径
+        :return: {字段名称：[字段类型，数据量，空值个数]}
+        """
+        info_dict = {}
+        data = pd.read_csv(path)
+        for c in data.columns:
+            ctype = data[c].dtype
+            nc = data[c].size - data[c].notnull().sum()
+            info_dict[c] = [ctype, data[c].size, nc]  # 字段类型，数据量，空值个数
+        return info_dict, data
+
+    def is_contain_empty_value(self, file_dict):
+        empty_col_list = []
+        for item in file_dict:
+            self.logger.info(file_dict[item])
+            if int(file_dict[item][2]) > 0:
+                self.logger.info("列" + item + "空值个数：" + str(file_dict[item][2]))
+                empty_col_list.append(item)
+        if len(empty_col_list) > 0:
+            return True, empty_col_list
+        else:
+            return False, []
+
+    def main(self):
+        file_path = input("请输入待处理的文件名路径：")
+        import os.path
+        if os.path.isfile(file_path):
+            file_dict, data = self.file_info(file_path)
+            is_contain_empty_value, empty_col_list = self.is_contain_empty_value(file_dict)
+            if is_contain_empty_value:
+                self.logger.info("当前存在缺失值")
+                is_fill_empty = self.console_input(prompt="是否需要填充数据？1：是，其他值：否", if_value=["1"], else_value=[],
+                                                   if_rtn=True, else_rtn=False)
+                if is_fill_empty:
+                    for col in empty_col_list:
+                        fill_value = input("请输入列" + col + "待填充的数据：")
+                        self.logger.info("列" + col + "将填充数据：" + fill_value)
+                        data = self.fill_empty_value(col_name=col, data=data, default_value=fill_value)
+                    print(data)
+                else:
+                    self.logger.info("不填充数据，程序退出")
+            else:
+                self.logger.info("当前不存在缺失数据")
+        else:
+            self.logger.error("指定的文件路径不存在")
 
 
 def run():
     ar = ARFilter()
-    ar.train_cal_input()
+    # ar.train_cal_input()
     # ar.fill_empty_value(col_name='emptyCol', file_name='empty.xls', default_value=0)
     # ar.del_empty_value(file_name="empty_ratio.xls")
+    ar.main()
+
 
 if __name__ == "__main__":
+
     run()
+
