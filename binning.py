e379cb9d2b414e69d69ddbe2b9c7b2da775c968b
Lansingcode <1406063770@qq.com>
2018-06-21 14:03:08 +0800

    add binning

--- binning.py
+++ binning.py
@@ -4,31 +4,31 @@ __author__ = 'xujia'
 import pandas as pd
 
 
-def equal_distance_binning(data, fea_name):
+def equal_distance_binning(df, fea_name):
     """
     等距分箱
-    :param data:
+    :param df:
     :param fea_name:
     :return:
     """
-    pass
+    df[fea_name + '_d'] = pd.cut(df[fea_name])
 
 
-def equal_frequency_binning(data, fea_name):
+def equal_frequency_binning(df, fea_name):
     """
     等频分箱
-    :param data:
+    :param df:
     :param fea_name:
     :return:
     """
-    pass
+    df[fea_name + '_f'] = pd.cut(df[fea_name])
 
 
-def auto_binning(data, fea_name):
+def auto_binning(df, fea_name):
     """
     自动分箱
-    :param data:
+    :param df:
     :param fea_name:
     :return:
     """
-    pass
+    df[fea_name + '_a'] = pd.cut(df[fea_name])
